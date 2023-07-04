4b9d1013276d7a836cd050d4d082cb4388f1fd1c
ahmedihabb2 <ahmedbaz14@gmail.com>
2023-07-04 13:52:26 +0300

    Initial Commit

---
+++ animal/animal.py
@@ -0,0 +1,13 @@
+class Animal:
+    weight = 50
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self):
+        return self.name +  "is eating."
+
+    def drink(self):
+        return self.name +  "is drinking."
+    
+    def get_name(self):
+        return self.name
---
+++ cat/cat.py
@@ -0,0 +1,5 @@
+from animal.animal import Animal
+
+cat = Animal("cat")
+cat_weight = Animal.weight
+cat_name = cat.get_name()
